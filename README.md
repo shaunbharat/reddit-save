0669b747e3a9e646691d02a8e5591af0cda1c32a
Shaun Bharat <79479981+shaunbharat@users.noreply.github.com>
2023-05-04 20:34:20 -0400

    Add source code
    - Initial commit that adds the main source code.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # reddit-save
-Saves Reddit post content.
+
+A simple script that saves Reddit post content.
+
+> **Note**:
+>
+> - Has only been tested on Windows (11) so far.
+> - The code is messy and inefficient. Not particularly going for quality here, just kind of getting it done before Imgur pulls a Tumblr on us on May 15, 2023.
+> - The script is opinionated. You may have to tinker with the code a bit, if the default behavior doesn't appeal to you.
+> - Updates might come, with more functionality and improved code quality, and added customizability.
+
+## Prerequisites
+
+- Reddit Data
+    1. Request your data from Reddit (GPDR) [here](https://www.reddit.com/settings/data-request). This might take a while, so it's recommended to do this in advance.
+    2. Download the zip archive once the download is ready.
+    3. Extract the zip archive and copy that folder into the root of this project.
+    4. Rename the folder to `data`.
+
+- Reddit App
+    1. Create a Reddit app [here](https://www.reddit.com/prefs/apps/). It should be a `personal use script`. The redirect URI doesn't matter; it can be set to anything. However, you'll have to come back to this page in the next (Config) prerequisite though, to get your client ID and client secret, and to generate your refresh token. To get your refresh token, you'll need to change the redirect URI. So, just change it now instead.
+    2. Set your Reddit app's redirect URI to `https://not-an-aardvark.github.io/reddit-oauth-helper/`.
+    3. Make note of your Reddit app's client ID and client secret. You'll need them in the next (Config) prerequisite.
+
+- Config
+    1. Run the script once to generate the default config: `npm start`.
+    2. Set `REDDIT_USERNAME` to your Reddit username, in the `.env` file.
+    3. Set `REDDIT_CLIENT_ID` to your Reddit app's client ID, in the `.env` file.
+    4. Set `REDDIT_CLIENT_SECRET` to your Reddit app's client secret, in the `.env` file.
+    5. Ensure your Reddit app's redirect URI is set to `https://not-an-aardvark.github.io/reddit-oauth-helper/`. This is a tool that helps you generate a refresh token.
+    6. Head over [here](https://not-an-aardvark.github.io/reddit-oauth-helper/) in your browser
+    7. Under "Generate Token", input your client ID and client secret.
+    8. Check the "Permanent?" box, or leave it blank and it will expire in an hour. You can manually revoke tokens when you're done with them by sending a request to `https://www.reddit.com/api/v1/revoke_token` (see the [Wiki](https://github.com/reddit-archive/reddit/wiki/OAuth2#manually-revoking-a-token) for more), or by visiting the installed apps page [here](https://www.reddit.com/prefs/apps/).
+    9. Check all the scope boxes. The script really only requires the `read` scope, but you can check all of the boxes to reduce the risk of any errors.
+    10. Click the "Generate Token" button.
+    11. Copy, then set `REDDIT_REFRESH_TOKEN` to your generated refresh token, in the `.env` file.
+
+- Gallery-dl (used for downloading media content)
+    1. Download the appropriate `gallery-dl` binary for your system [here](https://github.com/mikf/gallery-dl/releases), and place it in the root of this project. Alternatively, you can install it with `pip install -U gallery-dl`, if you have Python 3 installed.
+    2. Rename the binary to `gallery-dl` (or `gallery-dl.exe` on Windows), if necessary.
+    3. Run `gallery-dl oauth:reddit` and follow the instructions on the opened webpage to authenticate your Reddit account with gallery-dl.
+
+## Usage
+
+> **Note**:
+> Edit the `Main` or `DownloadAll` function in `index.ts` to change the default behavior (for example, only download posts) to your liking.
+
+1. Install dependencies: `npm install`.
+2. Prepare your Reddit data, as described in the prerequisites.
+3. Prepare your Reddit app, as described in the prerequisites.
+4. Prepare your config, as described in the prerequisites.
+5. Prepare gallery-dl, as described in the prerequisites.
+6. Run the script: `npm start`. If there any failed downloads at the end in `failed.csv`, retry them like this: `DownloadAll('posts/failed.csv')`. This does work sometimes for posts that didn't download the first time. Otherwise, they may have been deleted or removed, or the script may have failed to download them for some other reason.
+
+By default, the script will save all posts to the `posts` folder, organized with subdirectories for subreddits. Basic information will be saved to `post.json`, in the post's specific directory. A thumbnail will also be saved, if available.
+
+## License
+
+At the moment, this project is not licensed. All rights reserved.
---
+++ index.ts
@@ -0,0 +1,162 @@
+import snoowrap from 'snoowrap';
+import { parse } from 'csv/sync';
+import { Writer } from 'steno';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+import fs from 'node:fs';
+import os from 'node:os';
+import { exec } from 'node:child_process';
+
+import module from 'module';
+const require = module.createRequire(import.meta.url);
+const { version } = require('./package.json');
+
+// Load .env file, or generate one if it doesn't exist
+if (dotenv.config().error) {
+    console.log('No .env file found.\nGenerating default config...');
+    await new Writer('.env').write('REDDIT_USERNAME = ""\nREDDIT_CLIENT_ID = ""\nREDDIT_CLIENT_SECRET = ""\nREDDIT_REFRESH_TOKEN = ""\n').catch(
+        () => {
+            console.log('Failed to create default .env file.\nPlease create a .env file with the following environment variables: "REDDIT_USERNAME", "REDDIT_CLIENT_ID", "REDDIT_CLIENT_SECRET", "REDDIT_REFRESH_TOKEN".\n',);
+        }
+    );
+    process.exit(1);
+}
+
+const REDDIT_USERNAME = process.env.REDDIT_USERNAME;
+const REDDIT_CLIENT_ID = process.env.REDDIT_CLIENT_ID;
+const REDDIT_CLIENT_SECRET = process.env.REDDIT_CLIENT_SECRET;
+const REDDIT_REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const REDDIT_APP_NAME = 'reddit-save';
+const USER_AGENT = `${os.platform()}:${REDDIT_APP_NAME}:${version} (by /u/${REDDIT_USERNAME})`;
+
+const reddit = new snoowrap({
+    userAgent: USER_AGENT,
+    clientId: REDDIT_CLIENT_ID,
+    clientSecret: REDDIT_CLIENT_SECRET,
+    username: REDDIT_USERNAME,
+    refreshToken: REDDIT_REFRESH_TOKEN,
+});
+
+reddit.config({ continueAfterRatelimitError: true });
+
+const output_directory = 'posts';
+
+(async function Main() {
+    // await DownloadAll('posts/failed.csv');
+    await DownloadAll('data/hidden_posts.csv');
+})();
+
+interface Post {
+    data: {
+        id: string,
+        author: string,
+        title: string,
+        subreddit: string,
+        permalink: string,
+    };
+    content: {
+        thumbnail: string,
+        domain: string,
+        url: string,
+    };
+};
+
+/**
+ * GetLinks returns an array of post IDs from the given CSV file.
+ * Comments (#) and empty lines are ignored.
+ * It is assumed that the first column of the CSV file contains the post IDs,
+ * and that the first line of the CSV file is a header.
+ * @param filename - The CSV file to read from.
+ */
+async function GetLinks(filename: string): Promise<string> {
+    // Get array of CSV records
+    const records = await parse(fs.readFileSync(filename), {
+        from_line: 2,
+        skip_empty_lines: true,
+        comment: '#',
+    });
+
+    // Return array of post IDs
+    return records.map((record: string) => record[0]);
+}
+
+async function WritePostData(post_directory: string, data: Post) {
+    await new Writer(post_directory + '/post.json').write(JSON.stringify(data));
+}
+
+async function DownloadThumbnail(url: string, post_directory: string) {
+    const image = (await axios.get(url, { responseType: 'stream' })).data;
+    await new Writer(post_directory + '/thumbnail.jpg').write(image);
+}
+
+/**
+ * Downloads all post content from every Reddit post in the given CSV file.
+ * It takes in a CSV filename directly, removing the need to call GetLinks.
+ * @param filename - The CSV file to read from.
+ */
+async function DownloadAll(filename: string) {
+    const failed: string[] = [];
+    const posts = await GetLinks(filename);
+    let remaining = posts.length;
+
+    console.log('Starting downloads...\n\n---');
+
+    for (const id of posts) {
+
+        remaining--;
+
+        let Post: Post;
+        let submission: snoowrap.Submission;
+
+        try {
+            submission = reddit.getSubmission(id);
+            // if (!submission.over_18) { continue; } // only download NSFW posts (in case you need this, it's here for you to uncomment)
+            Post = {
+                data: {
+                    id: await submission.id,
+                    author: await submission.author.name,
+                    title: await submission.title,
+                    subreddit: await submission.subreddit_name_prefixed,
+                    permalink: await submission.permalink,
+                },
+                content: {
+                    thumbnail: await submission.thumbnail,
+                    domain: await submission.domain,
+                    url: await submission.url,
+                }
+            };
+        } catch (error) {
+            failed.push(id + ',' + 'https://www.reddit.com/' + id);
+            console.log(`[${id}] Failed. ${remaining} remaining.`);
+            continue;
+        }
+
+        try {
+            // Create post directory if it doesn't exist
+            const post_directory = output_directory + '/' + Post.data.subreddit + '/' + Post.data.id;
+            fs.mkdirSync(post_directory, { recursive: true });
+
+            // Write post data to file
+            await WritePostData(post_directory, Post);
+
+            // Download content
+            if (!Post.content.thumbnail.includes('http')) {
+                try { await DownloadThumbnail(Post.content.thumbnail, post_directory); } catch (error) { } // ignore thumbnail if it fails, or if there is no link
+            }
+            await exec(`gallery-dl ${Post.content.url} -D ${post_directory}`); // uppercase D for exact directory
+            console.log(`[${Post.data.id}] Downloaded. ${remaining} remaining.`);
+        } catch (error) {
+            failed.push(id + ',' + 'https://www.reddit.com' + Post.data.permalink); // no extra slash because permalink already has one
+            console.log(`[${Post.data.id}] Failed. ${remaining} remaining.`);
+            console.log(Post);// debug
+            console.log(error); // debug
+            continue;
+        }
+
+    }
+    // Write failed posts to file
+    await new Writer(`${output_directory}/failed.csv`).write('id,permalink\n' + failed.join('\n'));
+
+    console.log(`---\n\nDownloads complete. Failed posts: ${failed.length}`);
+}
